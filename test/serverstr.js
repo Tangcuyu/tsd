c9b68192d06247bfff69be1ba17d0488461ee3a4
tangcuyu <joysmshr@126.com>
2016-05-09 14:08:52 +0800

    add clearIntervals in test/serverstr.js

--- test/serverstr.js
+++ test/serverstr.js
@@ -3,24 +3,28 @@ var net = require('net');
 
 var x = 0;
 
-
+var max = 2;
 
 
 
 var client = net.connect({ port: 9099, host: '101.200.209.103' }, function() {
   
-  setInterval(function() {
+  var id = setInterval(function() {
 
+    x++;
 
     var val = ((Math.random() * 100) / 20).toString();
 
     client.write('100211,tnpt213,'+ val + ',6,lx 9600,lxzj,,,,,db,;');
 
+    if(x > max) {
+      clearInterval(id);
+      client.end();
+      console.log('Data transfer done.');}
+
   }, 1000);
 
-  setTimeout(function(){
-    client.end();
-  },5000);
+    
 
 });
 
