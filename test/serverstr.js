45511d53dab67c4ef12872c2444f5d9913819f3f
tangcuyu <joysmshr@126.com>
2016-05-12 14:29:19 +0800

    add test/server.js servertmp.js serverstr.js

--- test/serverstr.js
+++ test/serverstr.js
@@ -3,7 +3,7 @@ var net = require('net');
 
 var x = 0;
 
-var max = 2;
+var max = 1000;
 
 
 
@@ -11,28 +11,34 @@ var client = net.connect({ port: 9099, host: '101.200.209.103' }, function() {
   
   var id = setInterval(function() {
 
-    x++;
+            x++;
 
-    var val = ((Math.random() * 100) / 20).toString();
+            var val = ((Math.round(Math.random()*100 + x)) / 20).toString();
 
-    client.write('100211,tnpt213,'+ val + ',6,lx 9600,lxzj,,,,,db,;');
+            client.write('100211,噪音传感器：tnpt213,'+ val + ',6,lx 9600,lxzj,,,,,db,;');
+            //client.write('{100211,tnpt213};');
 
-    if(x > max) {
-      clearInterval(id);
-      client.end();
-      console.log('Data transfer done.');}
+
+            if(x > max) {
+              clearInterval(id);
+              console.log('Data transfer done.');
+              client.end();
+              
+            }
 
   }, 1000);
-
     
+    client.setEncoding('utf8');
+    client.on('data', (chunk) => {
+                //assert.equal(typeof chunk, 'string');
+              //console.log('got data %d', chunk.toString());
+                console.log('got data:', chunk.toString());
+                });
+    client.on('end', () => {
+        console.log('disconnected from server');
+      });
 
 });
 
 
 
-client.setEncoding('utf8');
-client.on('data', (chunk) => {
-            //assert.equal(typeof chunk, 'string');
-          //console.log('got data %d', chunk.toString());
-            console.log('got data:', chunk.toString());
-            });
--- test/servertmp.js
+++ test/servertmp.js
@@ -15,10 +15,10 @@ var client = net.connect({ port: 9099, host: '101.200.209.103' }, function() {
 
     x++;
     //write({ key: '温度', value: (Math.random() + x) / 50 });
-    write({ key: '齿轮箱轴向振动', value: (Math.round(Math.random()*100)) / 30 });
+    //write({ key: 'X轴振动偏移', value: (Math.round(Math.random()*100)) / 30 });
     //write({ key: '温度', value: (Math.random() + x) / 50 });
     //write({ key: 'ohai', value: 1000 });
-    write({ key: '主轴承径向振动', value: (Math.round(Math.random()*100+25)) / 20});
+    write({ key: '温度', value: (Math.round(Math.random()*100/20))});
     //write({ key: '温度', value: (Math.random() + x) / 50 });
     
 
